0a2fadcbdc30f78e456e614d45c8cbf77c8f67d1
Dave <surigaojohndave@gmail.com>
2025-11-15 21:50:09 +0800

    Lists and Tuples

--- FIrst.py
+++ FIrst.py
@@ -68,3 +68,39 @@ wa = "sads"
 sad =  omg + (wa,)
 print(sad, len(sad))
 #Output = (("dave", 1, "john",2,3,4,5,6, "sads") 9
+
+#Nested Tupples
+tuples1 = (1,2,3,("four", "five","seven","eight","nine"),(4,5))
+tuples2 = tuples1[3][2]
+print(tuples2,"length:",len(tuples2))#Select from index 3 and collect value 2(seven) slice by 2
+#Output 5
+
+#Tupples LISTS
+tuples1 = [1,2,3,["four", "five"],["six", "seven"],[4,5]]
+print(tuples1[3])
+#Output = ["four", "five"]
+
+#Tupples Lists Extend
+tuples1 = [1,2,3,["four", "five"],["six", "seven"],[4,5]]
+tuples1.extend(["HELLO"])
+print(tuples1[6][:3])
+#Output = HEL
+
+#Tupples List muttable (change)
+tuples1 = [1,2,3,["four", "five"],["six", "seven"],[4,5]]
+tuples1[3] = "FOUR"
+print(tuples1)
+#Output = [1, 2, 3, 't', ['six', 'seven'], [4, 5]]
+
+#delete 
+tuples1 = [1,2,3,["four", "five"],["six", "seven"],[4,5]]
+del tuples1[1]     # removes the element at index 1
+print(tuples1)
+#Output = [1, 3, ['four', 'five'], ['six', 'seven'], [4, 5]]
+
+#Seperate strings into seperate character
+tuples1 = "a,b,c,d" #Data should already have , each index lol
+tuples2 = tuples1.split(",")
+tuples3 = tuples2 + [3]
+print(tuples3)
+#Output = ['a', 'b', 'c', 'd', 3]
