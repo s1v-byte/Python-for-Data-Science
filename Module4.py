46b88a75019e0bb503ecb17a93b87293a5465dd8
Dave <surigaojohndave@gmail.com>
2025-12-01 19:05:40 +0800

    more writing files with Open

--- Module4.py
+++ Module4.py
@@ -23,10 +23,42 @@ with open("testing.txt","r") as file1:
         for line in file1:
             print("Iteration", str(i), ": ", line)
             i = i + 1
-
+print("\n")
+       
 #Output:
 """
 this 
 is line
  1
 """
+#
+"""
+with open("testing2.txt", "w") as File2:
+    File2.write("This is line A")
+    File2.write("\n This is line B")
+"""
+#Creating new file if non-existent*
+"""
+Output: 
+This is line A
+ This is line B
+"""
+
+Lines = ["This is 1\n", "This is 3\n", "This is 3\n"]
+with open("testing2.txt", "w") as File2:
+
+    for line in Lines:
+        File2.write(line)
+
+#Writing file with list into for loop
+
+with open("testing2.txt", "a")as File2:
+
+    File2.write("This is append")
+
+
+#Copying existing file into a new file
+with open("testing3.txt", "r") as readfile:
+    with open("testing4.txt", "w") as writefile:
+        for line in readfile:
+            writefile.write(line)
