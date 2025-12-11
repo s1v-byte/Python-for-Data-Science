64d4df8ddd08e1cc03357db7035512a9018d1966
Dave <surigaojohndave@gmail.com>
2025-12-11 23:45:03 +0800

    Introduction to Numpy

---
+++ Module5.py
@@ -0,0 +1,24 @@
+"""
+In this lesson you will learn about:
+Explain the use of the HTTP protocol using the Requests Library method
+Describe how the URL Request Response HTTP protocol works
+Learn to apply simple, open-source APIs
+Perform basic webscraping using Python
+Work with different file formats using Python
+Explain the difference between APIs and REST APIs
+Summarize how APIs receive and send information
+"""
+
+import numpy as np
+arr = np.array([1, 2, 3, 4]) #Python List
+mat = np.array([[1, 2], [3, 4]]) #D array
+
+np.array([1, 2, 3])                # 1-D array
+np.array([[1, 2], [3, 4]])         # 2-D array
+
+np.zeros((3, 3))                   # 3x3 zeros
+np.ones((2, 4))                    # 2x4 ones
+np.full((2, 2), 5)                 # filled with 5
+
+np.arange(0, 10, 2)                # [0,2,4,6,8]
+np.linspace(0, 1, 5)               # 5 numbers 0→1
