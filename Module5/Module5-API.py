ba969fea2f7a2bd868e82a5abae08473334628f6
Dave <surigaojohndave@gmail.com>
2025-12-19 19:14:35 +0800

    Learning API #2

--- Module5/Module5-API.py
+++ Module5/Module5-API.py
@@ -63,7 +63,7 @@ data = {
 df = pd.DataFrame(data)
 #The data in dict_ is pass onto Pandas API
 # Display the first 3 rows
-print(df.head(3))
+df.head(3)
 #Default output for .head() is 5 columns/rows
 """
 Output:
@@ -128,4 +128,101 @@ Analyst-level mindset
 -Log failures
 -Automate pulls
 """
+#===============================================
+#-----------------------------------------------
+#===============================================
 
+#API PART 2
+
+#Random User Generator API
+    #Data is from randomuser (python library)
+from randomuser import RandomUser
+import pandas as pd
+
+"""r = RandomUser()
+some_list = r.generate_users(10)
+name = r.get_full_name()
+
+for user in some_list:
+    print (user.get_full_name()," ",user.get_email())"""
+
+#Exercise#1
+
+"""from randomuser import RandomUser
+import pandas as pd
+def get_users():
+    users = []
+    for user in RandomUser.generate_users(10):
+        user_dict = {
+            "Name": user.get_full_name(),
+            "Gender": user.get_gender(),
+            "City": user.get_city(),
+            "State": user.get_state(),
+            "Email": user.get_email(),
+            "DOB": user.get_dob(),
+            "Picture": user.get_picture()
+        }
+        users.append(user_dict)
+        print(user_dict)  # prints each appended record
+    return pd.DataFrame(users)
+df = get_users()
+#print(df)"""""
+
+#Another, more common way to use APIs, is through requests library. 
+#The next lab, Requests and HTTP, will contain more information about requests.
+
+import requests
+import pandas as pd
+url = "https://fruityvice.com/api/fruit/all"
+response = requests.get(url, timeout=10)
+response.raise_for_status()  # fails fast if network error
+data = response.json()
+df = pd.json_normalize(data)
+
+cherry = df[df["name"] == "Banana"]
+#print("Family:", cherry.iloc[0]["family"])
+#print("Genus:", cherry.iloc[0]["genus"])
+
+#Output: Family: Musaceae | Genus: Musa
+
+   #Using Dataframe
+import requests
+import pandas as pd
+# Fetch data from API
+url = "https://fruityvice.com/api/fruit/all"
+response = requests.get(url, timeout=10)
+response.raise_for_status()
+data = response.json()
+df = pd.json_normalize(data)
+slice_df = df.loc[1:4, ['name', 'family', 'genus']]
+
+print(slice_df)
+#===============================================
+#-----------------------------------------------
+#===============================================
+"""
+OUTLINE:
+- API KEYS AND ENDPOINTS
+- WATSON SPEECH TO TEXT
+- WATSON TRANSLATE
+"""
+   #API THAT USES AI
+#GOAL:
+   #transcibing audio file using Watson Text to Speech API
+   #Then translate the text to a new language using Watson Language-Translator API
+
+#PROCESS:
+#Transcribing Audio file into text
+#In the API CALL, you will send a copy of the audio file, <-- This is called POST REQUEST
+   #Then the API will send the text transcription of what the audio is saying <-- in the background, the API is making a GET REQUEST
+
+#Translating the Text into new language
+#We Then send the text we would like to translate into a second language to a second API
+#The API will translate the text and send the translation back to you, 
+
+
+"""
+Before doing the outline, Quick review of API KEYS and endpoints <-- this will give u access to the API
+- endpoint is the location of the service. its used to find the API on the Internet. just like web address
+- Many API will charge you for each call 
+"""
--- a-trythings.py
+++ a-trythings.py
@@ -1,25 +1,30 @@
-import numpy as np
+"""from ibm_watson import SpeechToTextV1
+url_s2t ="https://www.assemblyai.com/dashboard/activation"
+iam_apikey_s2t ="76d32f10b308471d8bfb28977b161010"
+s2t = SpeechToTextV1(iam_apikey_s2t, url=url_s2t)"""
 
-#A = np.array([[10, 20, 30],[40, 50, 60],[23, 23, 23]])
+"""import requests
+import os 
+from PIL import Image
+from IPython.display import IFrame
 
-#b = np.array([1, 2, 3])
+url='https://www.ibm.com/'
+r=requests.get(url)
+r.status_code
+r.text[0:100]
+header=r.headers
+print(r)"""
+
+#Random User Generator API
+    #Data is from randomuser (python library)
+from randomuser import RandomUser
+import pandas as pd
+
+r = RandomUser()
+some_list = r.generate_users(10)
+name = r.get_full_name()
+
+for user in some_list:
+    print (user.get_full_name()," ",user.get_email())
 
 
-#C= A.sum(axis=0)
-import numpy as np
-
-A = np.array([
-    [1, 2, 3],
-    [4, 5, 6]
-])          # shape (2, 3)
-B = np.array([
-    [1, 4],
-    [2, 5],
-    [3, 6]
-])          # shape (3, 2)
-print(np.dot(A,B))
-
-#Shape counts the rows and columns
-#Size counts all the index
-#Dot multiplies both the rows after addition
-#ndim counts all the brackets
